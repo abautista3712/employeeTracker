26ec7b7a14f23d584b5cf7ef4326f7ec0fceca1b
Abelard Bautista <abelardbautista@gmail.com>
2020-04-05 23:40:32 -0700

    Added NOT NULL to items in schema.

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE employee
 	id INT NOT NULL AUTO_INCREMENT,
 	first_name VARCHAR(30) NOT NULL,
 	last_name VARCHAR(30) NOT NULL,
-    role_id INT,
+    role_id INT NOT NULL,
     manager_id INT,
 	PRIMARY KEY (id)
 );
@@ -15,8 +15,8 @@ CREATE TABLE role
 (
 	id INT NOT NULL AUTO_INCREMENT,
 	title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    department_id INT
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
 	PRIMARY KEY (id)
 );
 
