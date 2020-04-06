52362bfcd8cfb8cde12226f8392e57bbd300c8cc
Abelard Bautista <abelardbautista@gmail.com>
2020-04-05 23:23:19 -0700

    Added role table.

--- schema.sql
+++ schema.sql
@@ -6,7 +6,16 @@ CREATE TABLE employee
 	id INT NOT NULL AUTO_INCREMENT,
 	first_name VARCHAR(30),
 	last_name VARCHAR(30),
-    role_id INT
-    manager_id INT
+    role_id INT,
+    manager_id INT,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE role
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
 	PRIMARY KEY (id)
 );
