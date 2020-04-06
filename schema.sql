c2f054777b4266e3b61b56420aa3b965fd2b1e33
Abelard Bautista <abelardbautista@gmail.com>
2020-04-05 23:33:51 -0700

    Added department table.

--- schema.sql
+++ schema.sql
@@ -4,8 +4,8 @@ USE employees_db;
 CREATE TABLE employee
 (
 	id INT NOT NULL AUTO_INCREMENT,
-	first_name VARCHAR(30),
-	last_name VARCHAR(30),
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
 	PRIMARY KEY (id)
@@ -14,8 +14,15 @@ CREATE TABLE employee
 CREATE TABLE role
 (
 	id INT NOT NULL AUTO_INCREMENT,
-	title VARCHAR(30),
+	title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE department
+(
+    id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(30) NOT NULL,
 	PRIMARY KEY (id)
 );
